bff94dd87ef530a2a1aeb62f2f7f7b0aba523637
AyushShende2679 <ayushshende2679@gmail.com>
2026-03-25 14:27:01 +0530

    Fix: config.js loading issue

---
+++ js/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAo-nfjs9lEUOJbN8k__YWE-cFj3GBTbts",
+  authDomain: "smart-attendance-cf1bb.firebaseapp.com",
+  projectId: "smart-attendance-cf1bb",
+  storageBucket: "smart-attendance-cf1bb.firebasestorage.app",
+  messagingSenderId: "625924538494",
+  appId: "1:625924538494:web:19574af5f71c6c9c01182f",
+  measurementId: "G-TK6W51TXTP",
+  databaseURL: "https://smart-attendance-cf1bb-default-rtdb.firebaseio.com"
+};
