40e0654fddd3f3171d0ccbf03d2caa7fec1dbecb
AyushShende2679 <ayushshende2679@gmail.com>
2026-03-26 15:57:17 +0530

    Delete attendance-app directory

--- attendance-app/js/firebase.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-import { getDatabase, ref, set, get, push, update, onValue, child } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
-
-// firebaseConfig is globally available from js/config.js
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-export { db, ref, set, get, push, update, onValue, child };
